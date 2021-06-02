68fe90f4548d8a0319260aa6e485ed9650a65472
Cesar Zamora Martinez <44296157+czammar@users.noreply.github.com>
2021-06-02 14:40:14 -0500

    typos

--- scripts/join_tables.R
+++ scripts/join_tables.R
@@ -3,15 +3,14 @@ list.of.packages <- c("dplyr", "tidyverse", "readxl")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 
-# tabla primera foto
+# tabla primera
 tabla1 <-  readxl_example("mi_archivo_1.xlsx", sheet = "conju")
 
-#tabla segunda foto
+#tabla segunda
 tabla2 <-  readxl_example("mi_archivo_2.xlsx", sheet = "Tipo2")
 
+# Join de las tablas
+df <- left_join(tabla1, tabla2, by = c("No.Cliente" = "cliente"))
 
-# Join de las tabla
-df <- left_join(tabla1, tabla, by = c("No.Cliente" = "cliente"))
-
-# escribe achivo a csv
+# escribe tabla con join a un archivo a csv
 write_csv(storms, "tabla_join.csv")
