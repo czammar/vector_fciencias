f24f08a926525f9f796196a366d249c542892b5a
Cesar Zamora Martinez <44296157+czammar@users.noreply.github.com>
2021-06-02 14:47:46 -0500

    fix wrong syntaxis of excel function

--- scripts/join_tables.R
+++ scripts/join_tables.R
@@ -4,10 +4,10 @@ new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"
 if(length(new.packages)) install.packages(new.packages)
 
 # lee la tabla de la hoja "conju", desde el archivo excel correspondiente
-tabla1 <-  readxl_example("mi_archivo_1.xlsx", sheet = "conju")
+tabla1 <-  read_excel("mi_archivo_1.xlsx", sheet = "conju")
 
 #lee la tabla de la hoja "Tipo2", desde el otro archivo excel
-tabla2 <-  readxl_example("mi_archivo_2.xlsx", sheet = "Tipo2")
+tabla2 <-  read_excel("mi_archivo_2.xlsx", sheet = "Tipo2")
 
 # Join de las tablas
 df <- left_join(tabla1, tabla2, by = c("No.Cliente" = "cliente"))
