873b90ff610f7f5596f0ae03ccb7cf59b8efc7cd
Cesar Zamora Martinez <44296157+czammar@users.noreply.github.com>
2021-06-02 14:42:55 -0500

    Update join_tables.R

--- scripts/join_tables.R
+++ scripts/join_tables.R
@@ -3,10 +3,10 @@ list.of.packages <- c("dplyr", "tidyverse", "readxl")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 
-# tabla primera
+# lee la tabla de la hoja "conju", desde el archivo excel correspondiente
 tabla1 <-  readxl_example("mi_archivo_1.xlsx", sheet = "conju")
 
-#tabla segunda
+#lee la tabla de la hoja "Tipo2", desde el otro archivo excel
 tabla2 <-  readxl_example("mi_archivo_2.xlsx", sheet = "Tipo2")
 
 # Join de las tablas
